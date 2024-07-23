fe3e6f0c246ed2df2d4b8d486120f6f7c63339ab
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-07-23 05:08:36 -0400

    second

--- update_after_submit/hooks.py
+++ update_after_submit/hooks.py
@@ -18,7 +18,7 @@ app_license = "MIT"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "update_after_submit/public/scss/website"
-
+x
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
 # webform_include_css = {"doctype": "public/css/doctype.css"}
